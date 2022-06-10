e3a48be21bdef1832a27827bc49e459aba472c82
FaultHappens <tskitishvili01@gmail.com>
2022-06-11 03:40:57 +0400

    Added login and registration page design and some functionality

--- app/src/main/java/com/example/anychat/presentation/ui/fragment/RegistrationFragment.kt
+++ app/src/main/java/com/example/anychat/presentation/ui/fragment/RegistrationFragment.kt
@@ -5,15 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.navigation.fragment.findNavController
 import com.example.anychat.R
+import com.example.anychat.databinding.FragmentLoginBinding
+import com.example.anychat.databinding.FragmentRegistrationBinding
 
 class RegistrationFragment : Fragment() {
+
+    lateinit var binding : FragmentRegistrationBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_registration, container, false)
+    ): View {
+        binding = FragmentRegistrationBinding.inflate(layoutInflater)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.registerBttn.setOnClickListener {
+
+        }
+
+        binding.loginNowBttn.setOnClickListener {
+            findNavController().navigate(R.id.loginFragment)
+        }
+
+        binding.passwordRepeatET.addTextChangedListener {
+            if(binding.passwordRepeatET.text == binding.passwordET.text){
+                //TODO: green
+            }else{
+                //TODO: red
+            }
+        }
+    }
 }
